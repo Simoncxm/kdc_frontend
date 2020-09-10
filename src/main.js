3e717bd8ceeb601d724dcbff5e2c9fc969d81f29
Secant1006 <secant1006@outlook.com>
2020-09-10 14:30:49 +0800

    Add video play function

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
+import axios from 'axios';
+import Video from 'video.js';
+import 'video.js/dist/video-js.css';
 import router from './router';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
+Vue.prototype.$axios = axios;
+Vue.prototype.$video = Video;
 
 new Vue({
   router,
---
+++ src/views/PlayVideo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <VideoPlayer/>
+  </div>
+</template>
+
+<script>
+import VideoPlayer from '@/components/VideoPlayer.vue';
+
+export default {
+  name: 'PlayVideo',
+  components: { VideoPlayer },
+};
+</script>
+
+<style scoped>
+
+</style>
