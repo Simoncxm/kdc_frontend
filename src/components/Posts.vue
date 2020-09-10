6371212803b7f56d4ea18bb03b01be63f8b49239
zxy990810 <719313055@qq.com>
2020-09-10 15:21:13 +0800

    帖子显示组件

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,12 +1,61 @@
 <template>
-       
-    <div> {{msg}}</div>
+    <div id="posts" v-if="posts.length">
+      <div v-for="post in posts" :key="post.id">
+        <el-card>
+          <el-avatar size="small" style="float: left"
+                     :src="post.user.avatar">
+          </el-avatar>
+          <span style="float: left">{{post.title}}</span>
+          <el-button style="float: right" type="danger">删除</el-button>
+<!--          v-if管理员权限-->
+          <el-tag style="float: right">学习讨论</el-tag>
+<!--          v-for-->
+        </el-card>
+      </div>
+      <el-pagination :current-page="page" background layout="prev, pager, next"
+      :total="total"></el-pagination>
+<!--      @current-change="changePage"-->
+    </div>
 </template>
+
 <style></style>
+
 <script>
-  export default {
-    data() {
-      return {msg: 'vue模板页'}
-    }
-  }
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          user: {
+            name: '张三',
+            avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+            id: 17373,
+          },
+          title: '道德的沦丧，还是人性的扭曲',
+        },
+        {
+          id: 2,
+          user: {
+            name: '张三',
+            avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+            id: 17373,
+          },
+          title: '道德的沦丧，还是人性的扭曲',
+        },
+        {
+          id: 1,
+          user: {
+            name: '张三',
+            avatar: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png',
+            id: 17373,
+          },
+          title: '道德的沦丧，还是人性的扭曲',
+        },
+      ],
+      total: 0,
+      page: 1,
+    };
+  },
+};
 </script>
