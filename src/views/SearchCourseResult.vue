21bff7375c82962a44141e76f4d9938bbe429a5b
JiPaa <2397886396@qq.com>
2020-09-16 10:00:44 +0800

    增加课程搜索功能

---
+++ src/views/SearchCourseResult.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Header/>
+    <CourseCards :courses="this.courses"/>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import CourseCards from '../components/course/CourseCards.vue';
+
+export default {
+  name: 'SearchCourseResult',
+  data() {
+    return {
+      searchText: '',
+      courses: null,
+    };
+  },
+  mounted() {
+    this.searchText = this.$route.query.text;
+    this.$axios.get(`/api/searchCourse/?key=${this.searchText}`)
+      .then((res) => {
+        if (res.data.code === -1) {
+          this.$notify({
+            title: '查询失败',
+            message: res.data.msg,
+            type: 'warning',
+          });
+        } else {
+          this.courses = res.data.list;
+        }
+      });
+  },
+  components: { CourseCards, Header },
+};
+
+</script>
