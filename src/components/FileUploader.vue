2fa1c9c93da3f73e4870213c107e39c04dbdf298
Secant1006 <secant1006@outlook.com>
2020-09-17 23:26:59 +0800

    添加进度记录功能

--- src/components/FileUploader.vue
+++ src/components/FileUploader.vue
@@ -51,6 +51,29 @@ export default {
           message: '请输入视频标题',
           type: 'warning',
           showClose: true,
+          duration: 2000,
+        });
+        return;
+      }
+      if (this.$refs.fileinput.files[0].size > 2 * 1024 * 1024 * 1024) {
+        this.$message({
+          message: '文件大小超过2G，不能上传',
+          type: 'error',
+          showClose: true,
+        });
+        return;
+      }
+      let existFlag = false;
+      this.$axios.get(`/api/video/getVideoExist?courseid=${this.$route.query.courseId}&title=${this.title}&hash=${this.hash}`)
+        .then((res) => {
+          existFlag = res.data !== false;
+        });
+      if (existFlag) {
+        this.$message({
+          message: '视频标题重复或文件内容重复',
+          type: 'error',
+          showClose: true,
+          duration: 2000,
         });
         return;
       }
@@ -75,6 +98,7 @@ export default {
               message: '上传失败',
               type: 'error',
               showClose: true,
+              duration: 2000,
             });
           } else {
             this.$axios.get(`/api/video/addVideoToCourse?courseId=${this.$route.query.courseId}&videoId=${res.data}`);
@@ -82,6 +106,10 @@ export default {
               message: '上传成功',
               type: 'success',
               showClose: true,
+              duration: 2000,
+              onClose() {
+                window.location.reload();
+              },
             });
           }
         });
